7d72ab8de06b66e2fc86363119b3d9f77c5a2fe8
John Wu <johnwu93@gmail.com>
2018-01-03 18:07:37 -0800

    Completed Assignment 2

---
+++ src/HW2/LogAnalysis.hs
@@ -0,0 +1,55 @@
+module HW2.LogAnalysis where
+
+import Data.Maybe (fromMaybe)
+import Text.Read (readMaybe)
+
+import HW2.Log
+
+parseMessage :: String -> LogMessage
+parseMessage message = fromMaybe (Unknown message) result
+  where
+    result = do
+      parsedMessages <- Just (words message)
+      (messageType, remainingMessages) <- parseMessageType parsedMessages
+      (timeStamp, logMessages) <- parseTimeStamp remainingMessages
+      return $ LogMessage messageType timeStamp $ unwords logMessages
+
+parseMessageType :: [String] -> Maybe (MessageType, [String])
+parseMessageType ("I":tail) = Just (Info, tail)
+parseMessageType ("W":tail) = Just (Warning, tail)
+parseMessageType ("E":maybeSeverity:tail) = do
+  severity <- readMaybe maybeSeverity
+  return (Error severity, tail)
+parseMessageType _ = Nothing
+
+parseTimeStamp :: [String] -> Maybe (TimeStamp, [String])
+parseTimeStamp parsedMessages =
+  case parsedMessages of
+    maybeTimestamp:logMessage ->
+      readMaybe maybeTimestamp >>= \timeStamp -> Just (timeStamp, logMessage)
+    _ -> Nothing
+
+parseMessages :: String -> [LogMessage]
+parseMessages messages = map parseMessage $ lines messages
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) messageTree = messageTree
+insert logMessage Leaf = Node Leaf logMessage Leaf
+insert insertedLogMessage (Node left currLogMessage right) =
+  if getTimeStamp insertedLogMessage <= getTimeStamp currLogMessage
+    then Node (insert insertedLogMessage left) currLogMessage right
+    else Node left currLogMessage (insert insertedLogMessage right)
+
+(<->) = flip insert
+
+build :: [LogMessage] -> MessageTree
+build = foldl (<->) Leaf
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node l logMessage r) = inOrder l ++ [logMessage] ++ inOrder r
+
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong logMessages = let filteredMessages = filter ((> 50) . getTimeStamp) logMessages in
+    map getMessage filteredMessages
---
+++ test/HW2/LogAnalysisSpec.hs
@@ -0,0 +1,91 @@
+module HW2.LogAnalysisSpec
+  ( main
+  , spec
+  ) where
+
+import HW2.Log
+import HW2.LogAnalysis
+
+import Test.Hspec
+  ( Spec
+  , anyException
+  , describe
+  , hspec
+  , it
+  , shouldBe
+  , shouldThrow
+  )
+
+import Test.QuickCheck
+import Data.List (sort)
+
+instance Arbitrary LogMessage where
+    arbitrary = do
+        errorIntensity <- choose (1, 100)
+        messageType <- elements [Error errorIntensity, Warning, Info]
+        timeStamp <- arbitrary
+        message <- arbitrary
+        return $ LogMessage messageType timeStamp message
+
+checkTreeOrder :: MessageTree -> Bool
+checkTreeOrder messageTree = checkTreeOrderWithBounds messageTree minBound maxBound where
+    checkTreeOrderWithBounds Leaf _ _ = True
+    checkTreeOrderWithBounds (Node  l (LogMessage _ timeStamp _) r) minTimeValue maxTimeValue =
+        let isValidTimeStamp = minTimeValue <= timeStamp && timeStamp <= maxTimeValue
+            leftSubResult = checkTreeOrderWithBounds l minTimeValue timeStamp
+            rightSubResult = checkTreeOrderWithBounds r timeStamp maxTimeValue in
+        isValidTimeStamp && leftSubResult && rightSubResult
+
+checkInOrder :: [LogMessage] -> Bool
+checkInOrder logMessages = let timeStamps = map getTimeStamp logMessages in
+    sort timeStamps == timeStamps
+
+
+createNode :: Int -> LogMessage
+createNode n = LogMessage Info n "hello"
+
+
+main :: IO ()
+main = hspec spec
+
+treeGenerator :: Gen MessageTree
+treeGenerator = do
+    treeSize <- choose (0, 20)
+    logMessages <- vector treeSize
+    return $ build logMessages
+
+sortedMessageGenerator :: Gen [LogMessage]
+sortedMessageGenerator = do
+    tree <- treeGenerator
+    return $ inOrder tree
+
+spec :: Spec
+spec =
+  describe "LogAnalysis" $ do
+    describe "parser" $ do
+      it "should parse error messages correctly" $
+        parseMessage "E 2 562 help help" `shouldBe`
+        LogMessage (Error 2) 562 "help help"
+
+      it "should parse info messages correctly" $
+        parseMessage "I 29 la la la" `shouldBe` LogMessage Info 29 "la la la"
+
+      it "should treat unparsable log messages as unknowns" $
+        parseMessage "This is not in the right format" `shouldBe`
+        Unknown "This is not in the right format"
+
+    describe "message tree" $ do
+      it "should never have a tree change state when an Unknown type is inserted" $
+        insert (Unknown "Hello") Leaf `shouldBe` Leaf
+
+      it "should create a new tree when a leaf is inserted" $ do
+         let actualResult = checkTreeOrder $ Leaf <-> createNode 8 <-> createNode 10 <-> createNode 12
+         actualResult `shouldBe` True
+
+      it "should have a list of nodes inserted into a BST" $ property $ forAll treeGenerator checkTreeOrder
+
+      it "should sort a list of nodes based on timeStamp" $ property $ forAll sortedMessageGenerator checkInOrder
+
+
+
+-- foldl (<->) Leaf [createNode 8, createNode 10]
